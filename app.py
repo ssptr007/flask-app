3dd314c346d232de80ea62a061efbaf29b7ca228
Sandeep R <34564325+ssptr007@users.noreply.github.com>
2025-06-03 16:47:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,34 +10,6 @@ def hello():
 def hi():
     return "Hi World!"
 
-@app.route('/greet')
-def greet():
-    return "Welcome Omkar"
-
-@app.route('/wish')
-def wish():
-    return "Welcome Sandeep"
-
-@app.route('/trip')
-def trip():
-    return "Welcome to Karma Experiences"
-
-@app.route('/place')
-def place():
-    return "Welcome to Goa"
-
-@app.route('/falls')
-def falls():
-    return "Welcome to Doodhsagar"
-
-@app.route('/guest')
-def Guest():
-    return "Welcome to Nithin sir"
-
-@app.route('/cost')
-def cost():
-    return "Cost is 2999"
-
 
 
 if __name__ == "__main__":
